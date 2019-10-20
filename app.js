7fcd1397813a03a86c852ac43180f57e5396a36c
Your Name <IamArnab01>
2019-10-20 13:05:38 +0530

    separate pages for new composed post created,

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const app = express();
 
@@ -14,6 +15,12 @@ const homeStartingContent="Lacus vel facilisis volutpat est velit egestas dui id
 const aboutContent="Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent="Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+// function truncateString(str, num) {
+//   if (str.length <= num) {
+//     return str
+//   }
+//   return str.slice(0, num) + '...'
+// }
 app.get("/",function(req,res){
     res.render("home",{homeContent:homeStartingContent,postArray:composeArray});
 });
@@ -35,9 +42,26 @@ app.get("/contact",function(req,res){
    content:req.body.postContent
  };  
  composeArray.push(post);
- res.redirect("/"); 
- 
- }) 
+ res.redirect("/");  
+ }) ;
+
+ app.get("/posts/:postName",function(req,res){
+  //  console.log(req.params.postName);
+  const requestedTitle =_.lowerCase(req.params.postName);
+
+  composeArray.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if(storedTitle === requestedTitle){
+      res.render("post",{
+        title:post.title,
+        content:post.content
+      });
+    }
+
+  });
+   
+ });
 
 app.listen(3000,function(){
     console.log("server is running at port 3000");
